b40d96773e716338bb688de5a98d24db73e42069
RentedZone <73078380+RentedZone@users.noreply.github.com>
2021-01-20 22:12:12 -0500

    Add files via upload

---
+++ views/routes/index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const router = express.Router();
+
+function parseCookies (rc) {
+    let list = {};
+
+    rc && rc.split(';').forEach(function( cookie ) {
+        let parts = cookie.split('=');
+        list[parts.shift().trim()] = decodeURI(parts.join('='));
+    });
+
+    return list;
+}
+
+// Main Page
+router.get('/', (req, res) => {
+    res.render('./layouts/index', {active: {Home: true}});
+});
+
+// Events Page
+router.get('/events', (req, res) => {
+    res.render('./layouts/events', {active: {Event: true}});
+});
+
+// FAQ Page
+router.get('/faq', (req, res) => {
+    res.render('./layouts/faq', {active: {Faq: true}});
+});
+
+// Contact Page
+router.get('/contact', (req, res) => {
+    let msg = parseCookies(req.headers.cookie).msg;
+    if(msg === undefined) msg = '';
+    let isGood = true;
+    if (msg.includes('Uh oh')) isGood = false;
+
+    res.render('./layouts/contact', {active: {Contact: true}, msg: msg, good: isGood});
+});
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+module.exports = router;
---
+++ views/routes/utils.js
@@ -0,0 +1,35 @@
+const router = require('express').Router();
+const nodemailer = require('nodemailer');
+
+// gets all friend requests
+router.post('/send-message', async (req, res) => {
+    let {fName, lName, email, subjectLine, subject} = req.body;
+    let name = fName + " " + lName;
+
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        service: 'gmail',
+        secure: false,
+        auth: {
+            user: 'cushotgun@gmail.com',
+            pass: 'CuShotgun2021'
+        },
+        debug: false,
+        logger: true
+    });
+
+    let mail = {
+        from: 'cushotgun@gmail.com',
+        to: 'cutrapandskeet@gmail.com',
+        subject: subjectLine,
+        html:'<h>From: ' + email + '</h>' + '<br>' + '<p>Name: ' + name + '</p>' + '<p>Subject: ' + subject + '</p>'
+    }
+
+    transporter.sendMail(mail, function(err, info) {
+        if (err) return res.cookie('msg','Uh oh! Something went wrong! Please try again.', {maxAge: 1000}).redirect('/contact');
+        else return res.cookie('msg','Thanks for contacting us! We\'ll get back to you as soon as possible.', {maxAge: 1000}).redirect('/contact');
+    });
+});
+
+module.exports = router;
